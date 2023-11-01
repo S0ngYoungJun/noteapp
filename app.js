16d52eb03c1acdee4737dd77a6b915ecaf5539ab
songyoungjun <clesaaaaaa@gmail.com>
2023-11-01 11:23:50 +0900

    수정가능,삭제불가

--- app.js
+++ app.js
@@ -50,6 +50,65 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.put('/api/notes', (req, res) => {
+  const { id, title, content } = req.body;
+  if (!id || !title || !content) {
+    return res.status(400).json({ error: 'ID, title, and content are required.' });
+  }
+
+  fs.readFile(notesFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+
+    const notes = JSON.parse(data);
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    notes[noteIndex] = { id, title, content };
+
+    fs.writeFile(notesFilePath, JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'An error occurred while saving the note.' });
+      }
+      res.json(notes[noteIndex]);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  
+  fs.readFile(notesFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading notes:', err); // 에러 로그 추가
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+
+    const notes = JSON.parse(data);
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if (noteIndex === -1) {
+      console.error('Note not found:', id); // 노트를 찾지 못한 경우 로그 추가
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    notes.splice(noteIndex, 1); // 배열에서 노트 삭제
+
+    fs.writeFile(notesFilePath, JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error('Error saving notes:', err); // 에러 로그 추가
+        return res.status(500).json({ error: 'An error occurred while saving the notes.' });
+      }
+      res.json({ message: 'Note deleted successfully.' });
+    });
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const noteTitle = document.getElementById('noteTitle');
   const noteContent = document.getElementById('noteContent');
   const doneButton = document.getElementById('doneButton');
+  const deleteButton = document.getElementById('deleteButton');
   const noteContainer = document.getElementById('noteContainer');
 
   createButton.addEventListener('click', () => {
@@ -33,21 +34,99 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
   });
+  function generateUniqueId() {
+    const timestamp = new Date().getTime();
+    const random = Math.floor(Math.random() * 100); // 0에서 999 사이의 랜덤 숫자
+    return `note-${timestamp}-${random}`;
+  }
+  
 
   function displayNoteBox(note) {
     const noteBox = document.createElement('div');
     noteBox.className = 'note-box';
     noteBox.innerHTML = `<h3>${note.title}</h3><p>${note.content}</p>`;
-    noteContainer.appendChild(noteBox);
-
-    noteBox.addEventListener('click', () => {
-      editNote(note);
+    noteBox.id =  generateUniqueId()
+    noteBox.style.borderRadius="30px"
+    noteBox.setAttribute('data-note-id', note.id);
+    const contentParagraph = noteBox.querySelector('p');
+    contentParagraph.addEventListener('click', () => {
+      contentParagraph.classList.toggle('selected');
     });
+    noteBox.addEventListener('click', () => {
+      const id = noteBox.getAttribute('data-note-id');
+    noteModal.setAttribute('data-note-id', id);
+      editNote(noteBox);
+    });
+    noteContainer.appendChild(noteBox);
   }
 
-  function editNote(note) {
-    // 박스 클릭 시 수정 기능을 구현
-    // 여기에 수정할 수 있는 UI 및 기능을 추가하세요
+  deleteButton.addEventListener('click', () => {
+    const id = noteModal.getAttribute('data-note-id');
+    if (id) {
+      fetch(`/api/notes/${id}`, {
+        method: 'DELETE',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.message === 'Note deleted successfully') {
+            const noteBox = document.getElementById(id);
+            if (noteBox) {
+              noteContainer.removeChild(noteBox);
+            }
+            noteModal.style.display = 'none';
+          } else {
+            alert('Failed to delete the note.');
+          }
+        })
+        .catch((error) => {
+          console.error('Error deleting note:', error);
+          alert('An error occurred while deleting the note.');
+        });
+    }
+  });
+  
+  noteContainer.addEventListener('click', (event) => {
+    const noteBox = event.target.closest('.note-box');
+    if (noteBox) {
+      const id = noteBox.getAttribute('data-note-id');
+      noteModal.setAttribute('data-note-id', id);
+      editNote();
+    }
+  });
+  function editNote(noteBox) {
+    // const noteBox = document.querySelector(".note-box")
+    const editTitle = noteBox.querySelector('h3');
+    const editContent = noteBox.querySelector('p');
+
+    // 기존 내용을 폼에 설정
+    noteTitle.value = editTitle.textContent;
+    noteContent.value = editContent.textContent;
+
+    noteContainer.removeChild(noteBox); // 기존 노트 박스 제거
+
+    noteModal.style.display = 'block'; // 수정 폼 표시
+
+    doneButton.addEventListener('click', () => {
+      const updatedTitle = noteTitle.value;
+      const updatedContent = noteContent.value;
+
+      if (updatedTitle && updatedContent) {
+        fetch('/api/notes', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id: note.id, title: updatedTitle, content: updatedContent }),
+        })
+          .then((response) => response.json())
+          .then((updatedNote) => {
+            noteTitle.value = '';
+            noteContent.value = '';
+            noteModal.style.display = 'none';
+            displayNoteBox(updatedNote);
+          });
+      }
+    });
   }
 
   // 초기 노트 목록을 불러오기
